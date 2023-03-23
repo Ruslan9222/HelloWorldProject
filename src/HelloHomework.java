c53294d4f61974e7c59cfd1b439b26581ae75a76
Notebook <magog21@gmail.com>
2023-03-24 00:05:53 +0300

    HomeWorkLessons2

---
+++ src/HelloHomework.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HelloHomework {
+    public static void main(String[] args) {
+        System.out.println("This is my second homework");
+    Scanner input = new Scanner(System.in);
+    String user = input.nextLine();
+    String profession = "beginner javadeveloper";
+        System.out.println("Hello"+ " " +user + " " + profession);
+
+    }
+}
