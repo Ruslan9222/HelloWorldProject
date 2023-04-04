00796f34c4ed81d3635dbc6bb6089dd899829927
Notebook <magog21@gmail.com>
2023-04-05 01:14:07 +0300

    FirstHomeWork

--- src/by/ruslan/radevich/homework/lessons4/FirstHomeWork.java
+++ src/by/ruslan/radevich/homework/lessons4/FirstHomeWork.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class FirstHomeWork {
     public static void main(String[] args) {
-        SeventhTask();
+        seventhTask();
     }
 
-    private static void SeventhTask() {
+    private static void seventhTask() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of months");
         int months = input.nextInt();
@@ -18,19 +18,45 @@ public class FirstHomeWork {
         double percent = 0.07;
         System.out.println((deposit * percent) * months + (deposit * months));
 
-        SecondTaskLessons4();
+        secondTaskLessons4();
 
 
     }
 
-    private static void SecondTaskLessons4() {
+    private static void secondTaskLessons4() {
         int[] arr = new int[5];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number");
+        int number = input.nextInt();
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt();
-            System.out.println((arr[i]=0));//Создайте массив целых чисел
-            // (заполните или руками, или случайным образом).
-            // Везде в массиве вместо указанного числа поставьте 0. Недоконца понимаю условие задачи
+            arr[i] = random.nextInt(5);
+            System.out.println(Arrays.toString(arr));
+            if (arr[i] == number) {
+                System.out.println(arr[i] = 0);
+            }
+        }
+
+
+        Scanner input1 = new Scanner(System.in);
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        float average = 0;
+        System.out.println("Enter the size of the array");
+        int n = input1.nextInt();
+        int[] arr2 = new int[n];
+        Random random1 = new Random();
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = random1.nextInt(10);
+            if (arr2[i] > minValue) {
+                minValue = arr2[i];
+                System.out.println("minValue " + minValue);
+            }
+            if (arr2[i] < maxValue) {
+                maxValue = arr2[i];
+                System.out.println("maxValue " + maxValue);
+            }//здесь задача не доделана
+
         }
     }
 }
