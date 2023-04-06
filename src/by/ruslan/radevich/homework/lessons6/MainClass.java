3a18fec82ac39f6e06cb695b06e5f8f15f8757cb
Notebook <magog21@gmail.com>
2023-04-07 01:50:13 +0300

    HomeWorkLessons6

--- src/by/ruslan/radevich/homework/lessons6/MainClass.java
+++ src/by/ruslan/radevich/homework/lessons6/MainClass.java
@@ -3,12 +3,16 @@ package by.ruslan.radevich.homework.lessons6;
 public class MainClass {
     public static void main(String[] args) {
         CreditCard myCreditCard = new CreditCard(12343214,0);
+        CreditCard myGoldCard = new CreditCard(55555555,100);
+        CreditCard myPlatinumCard = new CreditCard(99999999,1000);
         myCreditCard.putOfMoney(100);
-        myCreditCard.putOfMoney(1099);
+        myGoldCard.putOfMoney(226);
         myCreditCard.balance();
-        myCreditCard.outOfMoney(50);
+        myPlatinumCard.outOfMoney(50);
         myCreditCard.balance();
         myCreditCard.cardInformation();
+        myPlatinumCard.cardInformation();
+        myGoldCard.cardInformation();
 
 
     }
