1c2b7e0ee30c4a7a18b0286b50becb76ff39e28a
Notebook <magog21@gmail.com>
2023-04-12 22:03:06 +0300

    Lessons8

---
+++ src/by/ruslan/radevich/lessons8/MainExample.java
@@ -0,0 +1,31 @@
+package by.ruslan.radevich.lessons8;
+
+public class MainExample {
+    public static void main(String[] args) {
+        Parent badParent = new Child();
+        final int myInt = 5;//значение можно записать только 1 раз или просто константа
+        badParent.setName("Tom");
+        System.out.println(badParent.getName());
+
+        BadParent superBadParent = new BadParent();
+        Personable personable = new BadParent();
+    }
+
+    /**
+     * this metod returns Child if i ask him
+     * @param isParent - helps me to chose
+     * @return - parametr of child
+     */
+    public static Parent getParent(boolean isParent){
+        if (isParent) {
+            return new Parent();
+        }else {
+            return new Child();
+        }
+    }
+    public static int fib(int param){
+        if (param==0||param==1) return 1;
+        return fib(-1)+fib(-2);
+    }
+
+}
---
+++ src/by/ruslan/radevich/lessons8/Personable.java
@@ -0,0 +1,6 @@
+package by.ruslan.radevich.lessons8;
+
+public interface Personable {
+    void setName(String name);
+    String getName();
+}
