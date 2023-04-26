4db4c32ec7234c8f4e4fb2e7920bcf27a38b84db
Notebook <magog21@gmail.com>
2023-04-26 19:14:24 +0300

    Lessons10

---
+++ src/by/ruslan/radevich/lessons10/Containsxample.java
@@ -0,0 +1,9 @@
+package by.ruslan.radevich.lessons10;
+
+public class Containsxample {
+    public static void main(String[] args) {
+        String example = "This is my example";
+        System.out.println(example.toLowerCase().contains("my"));
+        String ex = "1234 - abcd-5678";
+    }
+}
