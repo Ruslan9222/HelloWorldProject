9e194aaf19cad2033de25af3d3a2d7f1401e6847
Notebook <magog21@gmail.com>
2023-05-15 21:52:21 +0300

    ClassWorkLessons14

---
+++ src/by/ruslan/radevich/lessons14/MyBestInterface.java
@@ -0,0 +1,6 @@
+package by.ruslan.radevich.lessons14;
+@FunctionalInterface//говорим что будет 1 интерфеис
+public interface MyBestInterface {
+    void test();
+    //void test2(); больше ничего не добавить
+}
