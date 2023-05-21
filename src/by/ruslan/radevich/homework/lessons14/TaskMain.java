0bdf557a05445c46c2a2bc6d1a26af518e826a16
Notebook <magog21@gmail.com>
2023-05-21 20:07:24 +0300

    HomeWorkLessons14PartTwo

--- src/by/ruslan/radevich/homework/lessons14/TaskMain.java
+++ src/by/ruslan/radevich/homework/lessons14/TaskMain.java
@@ -3,10 +3,14 @@ package by.ruslan.radevich.homework.lessons14;
 import by.ruslan.radevich.lessons14.Fruit;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+
 
 public class TaskMain {
     public static void thirdTask() {
@@ -25,6 +29,12 @@ public class TaskMain {
                 System.out.println("The number is negative");
             }
         };
+
+    }
+    public static void sixthTask(){
+        List<String> text = new ArrayList<>();
+        text.add("Make a Function that accepts a sentence and counts the number of words in it");
+        Function<String, Integer> text1;
     }
 
 
