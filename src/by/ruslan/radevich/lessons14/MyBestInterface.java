50a0081d2f7798783abf0475ecc037533a61c675
Notebook <magog21@gmail.com>
2023-05-22 22:03:40 +0300

    Lessons16

--- src/by/ruslan/radevich/lessons14/MyBestInterface.java
+++ src/by/ruslan/radevich/lessons14/MyBestInterface.java
@@ -1,6 +1,7 @@
 package by.ruslan.radevich.lessons14;
 @FunctionalInterface//говорим что будет 1 интерфеис
 public interface MyBestInterface {
-    void test();
+    String test(String a);
+   // void test();
     //void test2(); больше ничего не добавить
 }
---
+++ src/by/ruslan/radevich/lessons16/User.java
@@ -0,0 +1,27 @@
+package by.ruslan.radevich.lessons16;
+
+public class User {
+    private final int age;
+    private final String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
