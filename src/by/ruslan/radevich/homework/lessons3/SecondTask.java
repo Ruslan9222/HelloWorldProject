f51cd653a900bcac3d01377a1f19a55ec9deedb7
Ruslan9222 <magog21@gmail.com>
2024-04-29 20:55:08 +0300

    work helo world

--- src/by/ruslan/radevich/homework/lessons3/SecondTask.java
+++ src/by/ruslan/radevich/homework/lessons3/SecondTask.java
@@ -2,6 +2,7 @@ package by.ruslan.radevich.homework.lessons3;
 
 import java.util.Scanner;
 
+
 public class SecondTask {
     public static void main(String[] args) {
 
@@ -12,6 +13,10 @@ public class SecondTask {
         System.out.println("Enter the number");
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
+        String b= "wqeq";
+        String c= "wqeq";
+        String c1 = new String(c);
+
         if (number > 100) {
             System.out.println("User is not right");
         } else {
@@ -20,6 +25,11 @@ public class SecondTask {
                 sum = sum + i;
             }
             System.out.println(sum);
+            System.out.println(b.equals(c));
+            System.out.println(b==c);
+            System.out.println(c1==c);
+            System.out.println(c1.equals(c));
+
         }
 
     }
